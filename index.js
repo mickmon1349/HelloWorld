a03bc123c4eb0322dd7f9cde3861585e765c9526
mickmon1349 <mickmon1349@google.com>
2025-10-23 16:30:17 +0800

    change send word4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World in ShangChung');
+  res.send('Hello World in Taiwan');
 });
 
 app.get('/api/info', (req, res) => {
