0af42f55992209825d86b0438b03f6471d40cddc
mickmon1349 <mickmon1349@google.com>
2025-10-28 13:57:43 +0800

    change send word 29

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World in Test28 ');
+  res.send('Hello World in Test29 ');
 });
 
 app.get('/api/info', (req, res) => {
